f5aeab9b5ce73f6145ce926628dcddd69614df44
Usuario <Usuario@CPD03-M12>
2022-09-27 12:06:36 -0300

    Adicionar arquivos de projeto.

---
+++ AppTemporal/AppTemporal/Model/Tempo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTemporal.Model
+{
+    internal class Tempo
+    {
+    }
+}
---
+++ AppTemporal/AppTemporal/Services/DataService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppTemporal.Services
+{
+    internal class DataService
+    {
+    }
+}
