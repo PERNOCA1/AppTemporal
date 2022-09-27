16de6587f7e4418a8119c16bca89657ba5ce930a
Usuario <Usuario@CPD03-M12>
2022-09-27 12:28:39 -0300

    metade das edicoes dos arquivos

--- AppTemporal/AppTemporal/Model/Tempo.cs
+++ AppTemporal/AppTemporal/Model/Tempo.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace AppTemporal.Model
 {
-    internal class Tempo
+    class Tempo
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Humidity { get; set; }
+        public string Visibility { get; set; }
+        public string Sunrise { get; set; }
+        public string Sunset { get; set; }
+
+
+        public Tempo()
+        {
+            this.Title = " ";
+            this.Description = " ";
+            this.Temperature = " ";
+            this.Wind = " ";
+            this.Humidity = " ";
+            this.Visibility = " ";
+            this.Sunrise = "";
+            this.Sunset = " ";
+        }
     }
 }
--- AppTemporal/AppTemporal/Services/DataService.cs
+++ AppTemporal/AppTemporal/Services/DataService.cs
@@ -2,9 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppTemporal.Model;
+
 namespace AppTemporal.Services
 {
-    internal class DataService
+    class DataService
     {
+        public static async Task<Tempo> GetPrevisaoDoTempo(string cidade)
+        {
+            string appId = "6bc9fdf60e7df1500cab1643db19a682";
+
+            string queryString = "https://home.openweathermap.org/api_keys" + cidade + "&units=metric" + "&appid=" + appId;
+            dynamic resultado = await getDataFromService(queryString).ConfigureAwait(false);
+
+            if (resultado["weather"] != null)
+            {
+                Tempo previsao = new Tempo();
+                previsao.Title = (string)resultado["name"];
+                previsao.Temperature = (string)resultado["main"]["temp"] + " C";
+                previsao.Wind = (string)resultado["wind"]["speed"] + " mph";
+                previsao.Humidity = (string)resultado["main"]["humidity"] + " %";
+                previsao.Visibility = (string)resultado["weather"][0]["main"] + " C";
+                DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0 );
+                DateTime sunrise = time.AddSeconds((double)resultado["sys"]["sunrise"]);
+
+
+            }
+        }
+
     }
 }
